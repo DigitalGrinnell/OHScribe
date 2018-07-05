159d0aef1cdc06b6002d49c1158518e5cfbbfb67
Mark McFate <mcfatem@grinnell.edu>
2018-07-05 13:45:28 -0500

    Working on it

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,7 @@ app = Flask(__name__)
 app.static_folder = 'static'
 app.config['UPLOAD_FOLDER'] = environ.get('OHSCRIBE_UPLOAD_FOLDER')
 app.config['SECRET_KEY'] = environ.get('OHSCRIBE_SECRET_KEY') or 'i-hope-you-never-guess-this'
-
-# print("UPLOAD_FOLDER is: {}".format(app.config['UPLOAD_FOLDER']))
-# print("SECRET_KEY is: {}".format(app.config['SECRET_KEY']))
+host = environ.get('OHSCRIBE_HOST_ADDR')
 
 bootstrap = Bootstrap(app)
 
@@ -20,5 +18,4 @@ from app import routes, errors, actions
 # Use the host's IP address per https://stackoverflow.com/questions/7023052/configure-flask-dev-server-to-be-visible-across-the-network
 # Always encapsulate the '.run' call per https://stackoverflow.com/questions/29356224/error-errno-98-address-already-in-use
 if __name__ == '__main__':
-  # app.run(host= '0.0.0.0', port=5000)    # for PROD
-  app.run(host= '127.0.0.1', port=5000)    # for DEV
+  app.run(host=host, port=5000)    # for PROD host='0.0.0.0' and for DEV host='127.0.0.1'
