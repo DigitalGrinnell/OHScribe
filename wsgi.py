c95f6f2cb82eeff7e6395f35bd5aa0f11bef4492
Digital-Grinnell <digital@grinnell.edu>
2022-03-18 15:53:14 -0500

    Removed offending from app... line

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 # per https://flask.palletsprojects.com/en/2.0.x/deploying/mod_wsgi/
 
-from app import app as application
+# from app import app as application
 
 activate_this = '/var/www/webroot/ROOT/venv/bin/activate'
 with open(activate_this) as file_:
