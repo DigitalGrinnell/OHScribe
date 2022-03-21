fe03049147a825b9420b08d55135023e466da0d1
Digital-Grinnell <digital@grinnell.edu>
2022-03-21 11:05:33 -0500

    Working through run-time errors 03

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -11,11 +11,11 @@ from flask_bootstrap import Bootstrap
 #from os import environ
 
 class Config(object):
-    SECRET_KEY = environ.get('SECRET_KEY') or 'you-will-never-guess'
-    UPLOAD_FOLDER = environ.get('UPLOAD_FOLDER') or '/tmp'
-    LOG_VERBOSITY = environ.get('LOG_VERBOSITY') or 'DEBUG'
-    HOST_ADDR = environ.get('HOST_ADDR') or '127.0.0.1'
-    BASIC_AUTH_PASSWORD = environ.get('ADMIN_PASSWORD') or 'p@$$w0rd'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or '/tmp'
+    LOG_VERBOSITY = os.environ.get('LOG_VERBOSITY') or 'DEBUG'
+    HOST_ADDR = os.environ.get('HOST_ADDR') or '127.0.0.1'
+    BASIC_AUTH_PASSWORD = os.environ.get('ADMIN_PASSWORD') or 'p@$$w0rd'
     CURRENT_FILE = 'TBD'
     DEBUG_TB_INTERCEPT_REDIRECTS = False
 
