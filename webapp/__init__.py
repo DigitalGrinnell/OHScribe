f18ce7d1b66320e5575f6777b8fd8f7669cbb268
Digital-Grinnell <digital@grinnell.edu>
2022-03-21 11:02:11 -0500

    Working through run-time errors 02

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,7 +6,19 @@ from flask import Flask
 from logging.handlers import RotatingFileHandler
 from flask_bootstrap import Bootstrap
 # from config import Config
-import config
+
+## Was previously in config.py
+#from os import environ
+
+class Config(object):
+    SECRET_KEY = environ.get('SECRET_KEY') or 'you-will-never-guess'
+    UPLOAD_FOLDER = environ.get('UPLOAD_FOLDER') or '/tmp'
+    LOG_VERBOSITY = environ.get('LOG_VERBOSITY') or 'DEBUG'
+    HOST_ADDR = environ.get('HOST_ADDR') or '127.0.0.1'
+    BASIC_AUTH_PASSWORD = environ.get('ADMIN_PASSWORD') or 'p@$$w0rd'
+    CURRENT_FILE = 'TBD'
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
 
 # Initialize the app... populate app.config[]
 app = Flask(__name__)
