a59e2ebb7647c0343dd2464d13b61fdcb52b32ab
Digital-Grinnell <digital@grinnell.edu>
2022-03-21 16:37:39 -0500

    Fixed?

--- ohscribe.py
+++ ohscribe.py
@@ -11,7 +11,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or '/tmp'
     LOG_VERBOSITY = os.environ.get('LOG_VERBOSITY') or 'DEBUG'
-    HOST_ADDR = os.environ.get('HOST_ADDR') or '0.0.0.0'  # 127.0.0.1 for DEV, 0.0.0.0 for PROD
+    # HOST_ADDR = os.environ.get('HOST_ADDR') or '0.0.0.0'  # 127.0.0.1 for DEV, 0.0.0.0 for PROD
     # HOST_ADDR = os.environ.get('MASTER_IP') or '0.0.0.0'
     BASIC_AUTH_PASSWORD = os.environ.get('ADMIN_PASSWORD') or 'p@$$w0rd'
     CURRENT_FILE = 'TBD'
@@ -45,6 +45,9 @@ if app.config['LOG_VERBOSITY'] == 'DEBUG':
   app.logger.setLevel(logging.DEBUG)
 
 app.logger.info('OHScribe startup with LOG_VERBOSITY = %s.', app.config['LOG_VERBOSITY'])
+
+if __name__ == '__main__':
+    app.run()
 
 # bootstrap = Bootstrap(app)
 # host = app.config['HOST_ADDR']
