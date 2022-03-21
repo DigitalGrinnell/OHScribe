3bc880ecbd8cb3a83c0badc72f2b5171d479c56f
Digital-Grinnell <digital@grinnell.edu>
2022-03-21 10:24:38 -0500

    Oops, forgot to save wsgi.py earlier

--- wsgi.py
+++ wsgi.py
@@ -8,12 +8,14 @@
 
 # per https://www.pythonanywhere.com/forums/topic/30131/
 
-import sys
+# import sys
+#
+# # add your project directory to the sys.path
+# project_home = '/var/www/webroot/ROOT/app/'
+# if project_home not in sys.path:
+#     sys.path = [project_home] + sys.path
+#
+# # import flask app but need to call it "application" for WSGI to work
+# from app import app as application  # noqa
 
-# add your project directory to the sys.path
-project_home = '/var/www/webroot/ROOT/app/'
-if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
-
-# import flask app but need to call it "application" for WSGI to work
-from app import app as application  # noqa
+from ROOT.webapp import app as application
