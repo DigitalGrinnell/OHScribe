c7fbe59cd48ab6d5826013660380a2232fc2aa52
Digital-Grinnell <digital@grinnell.edu>
2022-03-21 19:31:53 -0500

    Debugging

--- ohscribe.py
+++ ohscribe.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
 
 # from ohscribe import app
 # from flask import render_template, flash, redirect, url_for, request, send_file
-from forms import MainForm
+# from forms import MainForm
 # from actions import do_cleanup, do_transform, do_hms_conversion, do_speaker_tags, do_analyze, do_all, allowed_file
 # from werkzeug.utils import secure_filename
 # import sys
@@ -798,3 +798,53 @@ def method_not_allowed(error):
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('500.html'), 500
+
+
+
+
+
+## --------- forms.py ------------
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField
+
+class MainForm(FlaskForm):
+  # browse = FileField('Select an XML File to Upload')
+
+  actions = RadioField('Action', choices=[
+    ('cleanup','Clean-Up the XML'),
+    ('transform', 'Transform XML to IOH'),
+    ('convert','Convert hh:mm:ss to Seconds'),
+    ('speakers', 'Format Speaker Tags'),
+    ('analyze', 'Analyze Cue Times')])
+
+  all = SubmitField('Do All of the Above')
+  go = SubmitField('Do Single Action')
+  exit = SubmitField('Exit')
+
+#
+# class LoginForm(FlaskForm):
+#   username = StringField('Username', validators=[DataRequired()])
+#   password = PasswordField('Password', validators=[DataRequired()])
+#   # remember_me = BooleanField('Remember Me')
+#   submit = SubmitField('Sign In')
+#
+#
+#
+
+# exit = SubmitField('Exit')
+# transform = SubmitField('Transform')
+# convert = SubmitField('Convert hh:mm:ss to Seconds')
+# formatSpeakers = SubmitField('Format Speakers')
+# string = StringField('This field accepts a string', validators=[DataRequired()])
+#     password = PasswordField('This field accepts a password', validators=[DataRequired()])
+#     checkbox = BooleanField('This is a checkbox')
+#     file1 = FileField('Select any File')
+# #   file2 = FileField('Select an XML File', [validators.regexp(u'^[^/\\]\.xml$')])  #
+# #   file2 = FileField('Select an XML File', validators=[Regexp(u'^[^/\\]\.xml$')])  #
+#     button1 = SubmitField('This is button1')
+#     button2 = SubmitField('This is button2')
+#     button3 = SubmitField('This is button3')
+#     submit = SubmitField('This is the Submit button')
