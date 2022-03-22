1ee2259268ceb5c62c324a17eb907e1816e03695
Digital-Grinnell <digital@grinnell.edu>
2022-03-21 19:26:28 -0500

    Debugging

--- .webapp/simple_routes.py
+++
@@ -1,7 +0,0 @@
-from webapp import app
-
-## From https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
